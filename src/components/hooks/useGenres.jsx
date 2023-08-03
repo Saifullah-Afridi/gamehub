3d17c6dea30730771ece68cfd523a552bfc6d8e9
Saifullah-Afridi <saifullahafridi08@gmail.com>
2023-08-03 21:51:37 +0500

    updated

---
+++ src/components/hooks/useGenres.jsx
@@ -0,0 +1,26 @@
+const useGenres = () => {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, SetIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    SetIsLoading(true);
+    apiClient
+      .get("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        SetIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        SetIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { games, error, isLoading };
+};
+
+export default useGenres;
