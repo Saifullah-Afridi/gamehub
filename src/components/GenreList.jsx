ca613cf56cbf9ce6c9bb26338bc963dfdc8b38ee
Saifullah-Afridi <saifullahafridi08@gmail.com>
2023-08-04 13:57:53 +0500

    update

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,8 +1,33 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "./hooks/useGenres";
 
 const GenreList = () => {
   const { genres, error, isLoading } = useGenres();
+  if (isLoading) return <Spinner />;
+  // if (isLoading)
+  //   return (
+  //     <Card height="100%">
+  //       <CardBody>
+  //         <SkeletonText
+  //           noOfLines={10}
+  //           skeletonHeight={3}
+  //           spacing={10}
+  //         ></SkeletonText>
+  //       </CardBody>
+  //     </Card>
+  //   );
   return (
     <List>
       {genres.map((gen) => (
